a26b0703957c5f92f343ddccc6b2174c2b9ca5c7
JOJOtaro17 <143568674+JOJOtaro17@users.noreply.github.com>
2023-11-06 21:24:16 -0600

    Add files via upload

---
+++ EVA2_21_CONTINUE_2/src/eva2_21_continue_2/EVA2_21_CONTINUE_2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_21_continue_2;
+
+/**
+ *
+ * @author Joshua
+ */
+public class EVA2_21_CONTINUE_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i=1;i<15;i++){
+            //MODULO---%
+            if((i%2) !=0)
+                continue;
+            System.out.print(i+" - ");
+        }
+    }
+    
+}
