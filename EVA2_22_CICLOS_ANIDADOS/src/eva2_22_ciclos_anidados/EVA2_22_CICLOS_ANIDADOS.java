83dffe0e7760900d9e6d735252ad2d2f3b40d548
JOJOtaro17 <143568674+JOJOtaro17@users.noreply.github.com>
2023-11-06 21:24:58 -0600

    Add files via upload

---
+++ EVA2_22_CICLOS_ANIDADOS/src/eva2_22_ciclos_anidados/EVA2_22_CICLOS_ANIDADOS.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_22_ciclos_anidados;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joshua
+ */
+public class EVA2_22_CICLOS_ANIDADOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nume;
+        Scanner input = new Scanner(System.in);
+        System.out.println("introduce un numero");
+        nume=input.nextInt();
+        for(int i = nume; i >= 1; i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
